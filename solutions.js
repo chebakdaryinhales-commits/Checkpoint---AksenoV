5707737b3062158c6de86dd241f42afe92669661
VVVVV <chebakdaryinhales@gmail.com>
2026-02-06 12:32:17 +0500

    aaaa

--- solutions.js
+++ solutions.js
@@ -1,14 +1,20 @@
-function getLongestLength(str) {
+export default function getLongestLength(str) {
     let maxLength = 0;
     let start = 0;   //Че дальще то? Ща прсмотрим
     const seen = new Map();
 
-    for (let i = 0; i < structuredClone.length; i++){
+
+
+    for (let i = 0; i < str.length; i++){
         const char = str[i];
-                  // функции Map эсть ли похожий символ, true или false
+
+
         if (seen.has(char) && seen.get(char) >= start){
-            start = seen.get(char) //ГДЕ. убирает повтор
+            start = seen.get(char) + 1; //ГДЕ. убирает повтор
         }
+
+
+
         seen.set(char, i); //не оч понял но вроде эта штука одновляет значения
         maxLength = Math.max(maxLength, i - start + 1) // тоже странная дичь
 
@@ -17,4 +23,4 @@ function getLongestLength(str) {
     return maxLength;
 }
 
-getLongestLength('ВАСЯБАЛБЕСКОЛБАСАПОМИДОРЫ')
+getLongestLength(`fweitokkam`)
