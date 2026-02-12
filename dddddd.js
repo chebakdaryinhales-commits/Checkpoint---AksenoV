42dfcfee76fbebac640e39af5758b794274dfbff
VVVVV <chebakdaryinhales@gmail.com>
2026-02-12 16:21:01 +0500

    Update dddddd.js

--- dddddd.js
+++ dddddd.js
@@ -16,3 +16,4 @@ return result;
 console.log(flatten([]))
 console.log(flatten([1, [3, 2], 9]))
 console.log(flatten([1, [[2], [3]], [9]]))
+console.log(flatten([1, [[2], [3], [ 7, [34, [345, [66623]]]]], [9]]))
