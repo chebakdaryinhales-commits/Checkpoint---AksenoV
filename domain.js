b6522ad7ce5980de4e27bbd6bea73900e20f761d
VVVVV <chebakdaryinhales@gmail.com>
2026-02-16 12:21:56 +0500

    Update domain.js

--- domain.js
+++ domain.js
@@ -1,1 +1,13 @@
-import getDomainInfo from './domain.js';
+const getDomainInfo = (domain) => {
+    let scheme = 'http';
+    if (domain.startsWith('https://')) {
+    scheme = 'https';
+    }
+    const name = domain.replace('http://', '').replace('https://', '');
+    return { scheme, name };
+};
+
+// Использование функции (подставляем ссылку здесь)
+const info = getDomainInfo('https://yandex.ru');
+
+console.log(info);
