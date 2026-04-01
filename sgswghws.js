75e91a5dec5323091476853408246ab97fff0d53
VVVVV <chebakdaryinhales@gmail.com>
2026-04-01 13:43:40 +0500

    oul7

--- sgswghws.js
+++ sgswghws.js
@@ -157,3 +157,17 @@ var sortBy = function(arr, fn) {
 };
 
 console.log(5+5)
+
+const longestCommonPrefix = (strs) => {
+    if (!strs.length) return "";
+    strs.sort();
+
+    const first = strs[0];
+    const last = strs[strs.length - 1];
+    let i = 0;
+    while (i < first.length && first[i] === last[i]) {
+        i++;
+    }
+
+    return first.substring(0, i);
+};
